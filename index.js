b67e2721dbb015786a61ca2ea8865c407aa4c76d
Talha Arshad <talha.arshad@systemsltd.com>
2022-08-05 11:58:27 +0500

    extraReducers

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const icecreamActions = require('./features/icecream/icecreamSlice').icecreamAct
 
 console.log('Initial state ', store.getState())
 
-const unsubscribe = store.subscribe(() => {})
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated state ', store.getState())
+})
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
