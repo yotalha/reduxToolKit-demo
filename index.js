ac6fd6b932c7dc6606e2c8e2500e98eb3ab311ef
Talha Arshad <talha.arshad@systemsltd.com>
2022-08-05 11:34:29 +0500

    redux toolkit basics

---
+++ index.js
@@ -0,0 +1,21 @@
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+
+console.log('Initial state ', store.getState())
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated state ', store.getState())
+})
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restocked(5))
+
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(1))
+
+unsubscribe()
