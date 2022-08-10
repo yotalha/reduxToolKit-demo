4a6dac4a5ef12e7e55856afa23590dc15b34352c
Talha Arshad <talha.arshad@systemsltd.com>
2022-08-10 13:32:25 +0500

    async redux toolkit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
 
 console.log('Initial state ', store.getState())
 
@@ -8,14 +9,16 @@ const unsubscribe = store.subscribe(() => {
     console.log('Updated state ', store.getState())
 })
 
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(5))
+store.dispatch(fetchUsers())
 
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.restocked(1))
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(5))
 
-unsubscribe()
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(1))
+
+// unsubscribe()
